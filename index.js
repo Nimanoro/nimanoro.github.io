759465f83ef036d4b1798fd7c9514a5517fae508
nimanoro <nimanourozy@gmail.com>
2023-07-18 12:11:07 -0700

    "Ready to deploy"

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './main.css'
-import reportWebVitals from './reportWebVitals';
-import App from './App';
+import reportWebVitals from './my-app/src/reportWebVitals';
+import App from './my-app/src/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
