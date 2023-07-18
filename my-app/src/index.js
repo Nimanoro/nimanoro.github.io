fbba5e25098f7469453093ad5de1c024589f99c6
nimanoro <nimanourozy@gmail.com>
2023-07-18 12:33:10 -0700

    "Update homepage"

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './main.css'
-import reportWebVitals from './src/reportWebVitals';
-import App from './src/App';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
