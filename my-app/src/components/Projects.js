8f2cc69705851e594036fe73de350c93d44485af
nimanoro <nimanourozy@gmail.com>
2023-07-18 17:58:49 -0700

    "Update projects + about page"

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -9,10 +9,11 @@ export default function Projects() {
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
-          <h1 className="text-center sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+          <h1 className="text-center font-serif sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             My projects
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
@@ -20,14 +21,14 @@ export default function Projects() {
             <a
               href={project.link}
               key={project.image}
-              className="sm:w-1/2 w-100 p-4">
-              <div className="flex relative">
+              className="sm:w-1/2  p-4">
+              <div className="flex relative w-96 h-96 r p-4">
                 <img
                   alt="Project Picture"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute  object-fill inset-0 object-scale-down h-96 w-96 object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className=" px-8 py-10 z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
